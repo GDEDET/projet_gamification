197feed92f422b81f9bde1d0391869817e0031c0
damien.tricard <damien.tricard@neo-soft.fr>
2023-04-24 10:08:43 +0200

    First commit

---
+++ todogame/src/main/java/fr/neosoft/todogame/TodogameApplication.java
@@ -0,0 +1,13 @@
+package fr.neosoft.todogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodogameApplication.class, args);
+	}
+
+}
---
+++ todogame/src/test/java/fr/neosoft/todogame/TodogameApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.neosoft.todogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
