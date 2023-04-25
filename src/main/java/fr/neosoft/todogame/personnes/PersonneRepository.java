95c06d516a0dacb72636e24d6db6c99d4743e2e5
GDEDET <guillaume.dedet@neo-soft.fr>
2023-04-25 11:15:23 +0200

    Maj Initialisation Role + Admin + push mockito

--- src/main/java/fr/neosoft/todogame/personnes/PersonneRepository.java
+++ src/main/java/fr/neosoft/todogame/personnes/PersonneRepository.java
@@ -1,10 +1,16 @@
 package fr.neosoft.todogame.personnes;
 
 
+import fr.neosoft.todogame.auth.roles.Role;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface PersonneRepository extends CrudRepository<Personne, Long> {
 
     Personne findByEmail(String email);
+
+    List<Personne> findByRoles_Authority(String role);
 
 }
