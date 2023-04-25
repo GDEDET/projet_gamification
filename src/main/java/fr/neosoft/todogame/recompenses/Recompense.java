5884bfa048bdb53a29727a9394ed9e2e83c7c5c2
Damien TRICARD <damien.tricard@neo-soft.fr>
2023-04-25 09:51:22 +0200

    Ajout des récompenses et des prérequis de récompense

--- src/main/java/fr/neosoft/todogame/recompenses/Recompense.java
+++ src/main/java/fr/neosoft/todogame/recompenses/Recompense.java
@@ -1,6 +1,8 @@
 package fr.neosoft.todogame.recompenses;
 
+import fr.neosoft.todogame.recompenses.prerequisrecompense.PrerequisRecompense;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -14,5 +16,22 @@ public class Recompense {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "nom_recompense", nullable = false, length = 50)
+    @NotBlank
+    private String nomRecompense;
+
+    @Column(name = "est_badge", nullable = false)
+    @NotBlank
+    private Boolean estBadge;
+
+    @Column(name = "point_gagne", nullable = false)
+    @NotBlank
+    private int pointGagne;
+
+    @ManyToOne
+    @MapsId("id")
+    @JoinColumn(name = "id")
+    private PrerequisRecompense prerequisRecompense;
 
 }
--- src/main/java/fr/neosoft/todogame/recompenses/RecompenseController.java
+++ src/main/java/fr/neosoft/todogame/recompenses/RecompenseController.java
@@ -1,17 +1,88 @@
 package fr.neosoft.todogame.recompenses;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("recompenses")
+@Tag(name = "Recompense", description = "L'API des récompenses")
+@SecurityRequirement(name = "Bearer Authentication")
+@Secured("PERSONNE")
 public class RecompenseController {
-    // CRUD methods
-    // GET /recompense ecris la fonction coplito stp
 
+	private final RecompenseService recompenseService;
 
+	public RecompenseController(RecompenseService recompenseService) {
+		this.recompenseService = recompenseService;
+	}
 
-    // GET /recompense/{id}
+	@Operation(summary = "Afficher toutes les récompenses")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Récompenses trouvées",
+					content = @Content( array = @ArraySchema(schema = @Schema(implementation = Recompense.class))))
+	})
+	@GetMapping
+	public Iterable<Recompense> findAll() {
+		return recompenseService.findAll();
+	}
 
-    // POST /recompense
-    // PUT /recompense/{id}
-    // DELETE /recompense/{id}
+	@Operation(summary = "Créer une récompense")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Récompense créée",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = Recompense.class))})
+	})
+	@PostMapping
+	@Secured("ADMIN")
+	public Recompense save(@RequestBody Recompense recompense) {
+		return recompenseService.save(recompense);
+	}
+
+	@Operation(summary = "Met à jour une récompense")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Récompense mise à jour",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = Recompense.class))})
+	})
+	@PutMapping
+	@Secured("ADMIN")
+	public Recompense modifier(@RequestBody Recompense recompense) {
+		return recompenseService.update(recompense);
+	}
+
+	@Operation(summary = "Trouver une récompense via son Id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Récompense trouvée",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = Recompense.class))}),
+			@ApiResponse(responseCode = "400", description = "Id fourni invalide",
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Récompense non trouvée")
+	})
+	@GetMapping("{id}")
+	public Recompense findById(@Parameter(description = "Id de la récompense à afficher") @PathVariable Long id) {
+		return recompenseService.findById(id);
+	}
+
+	@Operation(summary = "Supprimer une récompense via son Id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Récompense supprimée"),
+			@ApiResponse(responseCode = "400", description = "Id fourni invalide",
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Récompense non trouvée")
+	})
+	@DeleteMapping("{id}")
+	@Secured("ADMIN")
+	public void deleteById(@Parameter(description = "Id de la récompense à supprimer") @PathVariable Long id) {
+		recompenseService.deleteById(id);
+	}
 }
