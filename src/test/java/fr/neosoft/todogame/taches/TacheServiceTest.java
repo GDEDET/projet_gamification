9a9758ff31417a6a5407d82326e620bfc6233eb1
gaetan.villeneuve <gaetan.villeneuve@neo-soft.local>
2023-04-27 14:41:50 +0200

    [TU] Test unitaires sur les taches

--- src/test/java/fr/neosoft/todogame/taches/TacheServiceTest.java
+++ src/test/java/fr/neosoft/todogame/taches/TacheServiceTest.java
@@ -1,65 +1,107 @@
 package fr.neosoft.todogame.taches;
 
 import fr.neosoft.todogame.defis.GestionDefiInterface;
+import fr.neosoft.todogame.defis_personnes.GestionDefiPersonneInterface;
 import fr.neosoft.todogame.personnes.Personne;
 import fr.neosoft.todogame.personnes.PersonneService;
+import fr.neosoft.todogame.utils.GestionPersonneAuthentifieInterface;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TacheServiceTest {
 
-//    @Mock
-//    private PersonneService personneService;
-//
-//    @Mock
-//    private GestionDefiInterface gestionDefiInterface;
-//
-//    @Mock
-//    private TacheRepository tacheRepository;
-//
-//    private GestionTacheInterface gestionTacheInterface;
-//
-//    private List<Tache> tachesList;
-//
-//    private Personne personne;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        this.gestionTacheInterface = new GestionTacheService(tacheRepository, personneService, gestionDefiInterface);
-//
-//        this.tachesList = Arrays.asList(
-//                new Tache(1L, "Tache 1", null, null, Priorite.BASSE, Difficulte.FACILE, Statut.A_FAIRE, 1),
-//                new Tache(2L, "Tache 2", null, null, Priorite.ELEVEE, Difficulte.FACILE, Statut.EN_COURS, 2),
-//                new Tache(3L, "Tache 3", null, null, Priorite.MOYENNE, Difficulte.MOYENNE, Statut.TERMINE, 3)
-//        );
-//
-//        this.personne = new Personne();
-//        this.personne.setId(1L);
-//        this.personne.setTaches(tachesList);
-//
-//        // Configurer le mock PersonneService pour renvoyer la personne créée ci-dessus lors de l'appel de la méthode findById
-//        when(personneService.findById(personne.getId())).thenReturn(personne);
-//    }
-//
-//    @Test
-//    public void findAllByUser_shouldReturnTacheOfUser()
-//    {
-//        // Act
-//        Personne personne = personneService.findById(1L);
-//        Iterable<Tache> taches = gestionTacheInterface.findAllByUser(1L);
-//
-//        //Assert
-//        assertEquals(personne, personneService.findById(1L));
-//        assertEquals(personne.getTaches(), taches);
-//    }
+    @Mock
+    private PersonneService personneService;
+
+    @Mock
+    private GestionDefiInterface gestionDefiInterface;
+
+    @Mock
+    private GestionDefiPersonneInterface gestionDefiPersonneInterface;
+
+    @Mock
+    private GestionPersonneAuthentifieInterface gestionPersonneAuthentifieInterface;
+
+    @Mock
+    private TacheRepository tacheRepository;
+
+    @InjectMocks
+    private GestionTacheService gestionTacheService;
+
+    private List<Tache> tachesList;
+
+    private Personne personne;
+
+    @BeforeEach
+    public void setUp() {
+        this.personne = TacheData.DUPOND_MICHEL;
+        this.tachesList = TacheData.addListTaches(personne);
+        this.personne.setTaches(tachesList);
+    }
+
+    @DisplayName("Test de la méthode findAllByUser")
+    @Test
+    public void findAllByUser_shouldReturnTacheOfUser()
+    {
+        // Arrange
+        when(gestionPersonneAuthentifieInterface.getPersonneAuthentifie()).thenReturn(personne);
+
+        // Act
+        Iterable<Tache> taches = gestionTacheService.findAllByUserConnected();
+
+        //Assert
+        assertEquals(this.tachesList, taches);
+        assertEquals(personne.getTaches(), taches);
+    }
+
+    @DisplayName("Test de la méthode terminerTache")
+    @Test
+    public void terminerTache_shouldReturnTacheTerminer()
+    {
+        // Arrange
+        when(tacheRepository.findById(this.tachesList.get(0).getId())).thenReturn(Optional.ofNullable(this.tachesList.get(0)));
+
+        // Act
+        gestionTacheService.terminerTache(this.tachesList.get(0).getId());
+        assertEquals(Statut.TERMINE, this.tachesList.get(0).getStatut());
+        assertNotNull(this.tachesList.get(0).getDateRealisation());
+    }
+
+    @DisplayName("Test de la méthode creerTache")
+    @Test
+    public void creerTache_shouldReturnTache()
+    {
+        // Arrange
+        TacheDto tacheDTO = new TacheDto("tache 1", new Date(), Priorite.MOYENNE, Difficulte.FACILE);
+
+        // Act
+        Tache tache = gestionTacheService.creerTache(this.personne, tacheDTO);
+
+        //Assert tache
+        assertEquals(tacheDTO.getDescription(), tache.getDescription());
+        assertEquals(tacheDTO.getDateEcheance(), tache.getDateEcheance());
+        assertEquals(tacheDTO.getPriorite(), tache.getPriorite());
+        assertEquals(tacheDTO.getDifficulte(), tache.getDifficulte());
+        assertEquals(Statut.EN_COURS, tache.getStatut());
+        assertEquals(tacheDTO.getDifficulte().getNbPoint() + tacheDTO.getPriorite().getNbPoint(), tache.getNbPoints());
+
+        //Assert personne
+        assertEquals(this.personne.getTaches().get(this.personne.getTaches().size() - 1), tache);
+        verify(personneService, times(1)).save(this.personne);
+    }
 }
